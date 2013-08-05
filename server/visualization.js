80819ec355e32991a39dc799b63a6e02bd4c8a89
Chris Friel <cfriel@gmail.com>
2013-08-05 15:29:04 -0400

    moving packages

--- server/visualization.js
+++ server/visualization.js
@@ -1,10 +1,16 @@
 var name = "visualization";
 var version = "1.0";
 
+visualization = function(){};
+
+visualization.prototype = new visualization();
+
+Visualization = visualization.prototype;
+
 Meteor.startup(function(){
     
     console.log("loading visualization package");
     
-    Greenlight.register_template(name, version);
+    Greenlight.register_template(name, version, Visualization);
     
 });
